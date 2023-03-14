7b510e4b744224c84ba924601619eec1a53974dc
Denis Stokin <stokindenis@gmail.com>
2023-03-15 01:56:59 +0300

    made the components "DialogItem" and "MessageItem"

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -1,45 +1,36 @@
 import s from './Dialog.module.css'
 import {NavLink} from "react-router-dom";
 
+function DialogItem(props){
+    return(
+        <div className={s.name}>
+            <NavLink to={"/dialog" + props.id}>
+                {props.nameInterlocutor}
+            </NavLink>
+        </div>
+    )
+}
+function MessageItem(props){
+    return(
+        <div className={s.message_text}>
+            {props.content}
+        </div>
+    )
+}
 function Dialog(){
     return(
         <div className={s.dialogs}>
             <div className={s.names}>
-                <div className={s.names}>
-                    <NavLink to="/dialog/1">
-                        Kolya Klimovich
-                    </NavLink>
-                    <div className={s.name}>
-                        <NavLink to="/dialog/2">
-                            Kiryl Shahnitsky
-                        </NavLink>
-                    </div>
-                    <div>
-                        <NavLink to="/dialog/3">
-                            Anton Grinkevich
-                        </NavLink>
-                    </div>
-                    <div className={s.name}>
-                        <NavLink to="/dialog/4">
-                            Ivan Ivanov
-                        </NavLink>
-                    </div>
-
-                </div>
+                <DialogItem nameInterlocutor={"Kolya"} id={"/1"}/>
+                <DialogItem nameInterlocutor={"Kiryl"} id={"/2"}/>
+                <DialogItem nameInterlocutor={"Leha"} id={"/3"}/>
+                <DialogItem nameInterlocutor={"Anton"} id={"/4"}/>
             </div>
             <div className={s.messages}>
-                <div className={s.message_text}>
-               Hello I'am Kolya
-                </div>
-                <div className={s.message_text}>
-                 how are you?
-                </div>
-                <div className={s.message_text}>
-                    i think it's best website
-                </div>
-                <div className={s.message_text}>
-                    Hello
-                </div>
+                <MessageItem content={"hello"}/>
+                <MessageItem content={"how are you?"}/>
+                <MessageItem content={"We'll meet tomorrow?"}/>
+                <MessageItem content={"Yes i can"}/>
             </div>
         </div>
     )
