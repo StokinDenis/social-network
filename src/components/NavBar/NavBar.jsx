37455d6b81b28c7a7e04cc74a64fff4e15a40281
Denis Stokin <stokindenis@gmail.com>
2023-03-21 12:22:30 +0300

    create friends bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,19 +1,50 @@
 import s from './NavBar.module.css'
 import {NavLink} from "react-router-dom";
+import ava1 from './../../image/ava1.jpg'
 
-function NavBar(){
-    return(
+function NavBar() {
+    return (
         <nav className={s.nav}>
             <ul>
-                <li> <NavLink to="/profile"  className = { navData => navData.isActive ? s.active : s.link } >Profile</NavLink></li>
-                <li> <NavLink to="/dialog" className = { navData => navData.isActive ? s.active : s.link }>Message</NavLink></li>
-                <li> <NavLink to="/news" className = { navData => navData.isActive ? s.active : s.link }>News</NavLink></li>
-                <li> <NavLink to="/musicPage" className = { navData => navData.isActive ? s.active : s.link }>Music</NavLink></li>
+                <li><NavLink to="/profile" className={navData => navData.isActive ? s.active : s.link}>Profile</NavLink>
+                </li>
+                <li><NavLink to="/dialog" className={navData => navData.isActive ? s.active : s.link}>Message</NavLink>
+                </li>
+                <li><NavLink to="/news" className={navData => navData.isActive ? s.active : s.link}>News</NavLink></li>
+                <li><NavLink to="/musicPage" className={navData => navData.isActive ? s.active : s.link}>Music</NavLink>
+                </li>
             </ul>
-            <div><NavLink to="/settingPage" className = {navData => navData.isActive ? s.active : s.link}>Setting</NavLink></div>
+            <div><NavLink to="/settingPage"
+                          className={navData => navData.isActive ? s.active : s.link}>Setting</NavLink></div>
             <div className={s.line}></div>
+            <div className={s.friends}>
+                <div className={s.friends__name_section}>
+                    Friends
+                </div>
+                <div className={s.friends__elements_container}>
+                    <div className={s.friends__element}>
+                        <div className={s.f}>
+                            <img className={s.friends__element_image} src={ava1} alt=""/>
+                        </div>
+                        <div className={s.friends__name_friend}>Alex</div>
+                    </div>
+                    <div className={s.friends__element}>
+                        <div className={s.f}>
+                            <img className={s.friends__element_image} src={ava1} alt=""/>
+                        </div>
+                        <div className={s.friends__name_friend}>Dimon</div>
+                    </div>
+                    <div className={s.friends__element}>
+                        <div className={s.f}>
+                            <img className={s.friends__element_image} src={ava1} alt=""/>
+                        </div>
+                        <div className={s.friends__name_friend}>Kolya</div>
+                    </div>
+                </div>
 
+            </div>
         </nav>
     )
 }
+
 export default NavBar;
